f3e7520c76e060a953f5633f59ef8d94a1e082d8
Razzer101 <Rasmus@anders-net.dk>
2025-10-02 10:24:11 +0200

    TextChanges

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ export default function Home(){
             <article>
                 <h2>What is it?</h2>
                 <p>React-Router is a library designed for React to make easier and more optimized navigation <br /> 
-                between pages on a website without the website having to refresh between pages.</p>
+                between pages on a website, without the website having to refresh between pages.</p>
             </article>
             <article>
                 <h2>How to install on Vite</h2>
@@ -35,22 +35,22 @@ export default function Home(){
             </article>
             <article>
                 <h2>Routing pages</h2>
-                <p>To route a page you'll have to make the file/pages you want the website be able to navigate to. The image below is an example where there's been made a About, Contact and Home .jsx file. <br />
-                Afterwards inside the Routes component you'll have to make a Route component as seen in the image.</p>
+                <p>To route a page you'll have to make the file/pages you want the website be able to navigate to. The image below is an example where there's been made an About, Contact and Home .jsx file. <br />
+                Afterwards inside the Routes component, you'll have to make a Route component as seen in the image.</p>
                 <p>The first Route has an index and element property.</p>
-                <p>Index is used to tell react-router which page should be yout main page/the page your website starts on. <br />
+                <p>Index is used to tell react-router which page should be your main page/the page your website starts on. <br />
                 Element is used to tell what file is conected to what page. <br />
-                Path which is used in the two other Route componentes is used to tell what name the endpoint of the page you are routing to should be called. The example path="about" in the image would then be "yourWebsite/about" in the Url.</p>
+                Path, which is used in the two other Route componentes, is used to tell what name the endpoint of the page you are routing to should be called. The example path="about" in the image would then be "yourWebsite/about" in the Url.</p>
                 <img src="../../public/ReactComponentRouting.png" alt="" />
             </article>
             <article>
                 <h2>Navigating between pages</h2>
-                <p>To be able to navigate between pages on your website you will need to make use of the Link component and/or useNavigate function. Both imported from "react-router" as seen in the example below. <br />
-                They are used in the files/pages where you want the user be able to navigate to and from other pages</p>
+                <p>To be able to navigate between pages on your website, you'll need to make use of the Link component and/or useNavigate function. Both imported from "react-router" as seen in the example below. <br />
+                They're used in the files/pages where you want the user be able to navigate to and from other pages</p>
                 <p>Link uses a to="" property where you write the endpoint the Link should move the user to on your website. The example below has written to="/contact" to move the user to yourWebsite/contact.</p>
                 <p>UseNavigate is a bit different. You use the function to move the user to another page when a "trigger" has happend. <br />
                 The trigger can be something the user does on the website. The example could be filling out a login. When the user has entered their username and password you then tell the useNavigate to move the user to another page.</p>
-                <p>In the example below there has been made a const named "navigate" = the useNavigate Function. <br />
+                <p>In the example below, there has been made a const named "navigate" = the useNavigate Function. <br />
                 Then there's made a button saying that on a click it should trigger the "navigate" that is written like navigate("/about") to tell it that it should navigate to yourWebsite/about.</p>
                 <img src="../../public/Link.png" alt="" />
                 <img src="../../public/UseNavigate.png" alt="" />
@@ -60,8 +60,8 @@ export default function Home(){
                 <h2>Error Page</h2>
                 <p>Lastly you'll have to make an error page. You start with making a file like the other page files. <br />
                 That will be the file/page your website sends the user to when they write an endpoint in the url that dosn't exist in your website.</p>
-                <p>As seen in the image below, you make another Route component with element = the file you want and the path that = "*". <br />
-                This means that every endpoint thats not = any endpoint you've already made should direct the user to the error page.</p>
+                <p>As seen in the image below, you make another Route component with element = the file you want, and path that = "*". <br />
+                The "*" means that every endpoint thats not any endpoint you've already made, should direct the user to the error page.</p>
                 <p>In the error page you can simply write whatever you feel necessary. In the video below there's just written Error in a h1 tag.</p>
                 <img src="../../public/Error.png" alt="" />
                 <video src={ErrorPageVid} muted controls/>
